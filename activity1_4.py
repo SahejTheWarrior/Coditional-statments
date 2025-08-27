3087d15c814c29ad955cffe626fe4157e8433e6f
SahejTheWarrior <sahejburande@gmail.com>
2025-08-27 11:23:22 +0100

    Add files via upload

---
+++ activity1_4.py
@@ -0,0 +1,11 @@
+#conditional statment
+#write a program to check wether the given number is positive
+
+num=17
+if num > 0:
+    print(num,"is a positive number")
+
+num=-1
+if num > 0:
+    print(num, "is a positive number")
+
